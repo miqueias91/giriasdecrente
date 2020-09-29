e7a9f66595dbd236978ffeabd78df7c58c9e5c50
miqueias91 <miqueiasmcaetano@gmail.com>
2020-09-28 21:52:28 -0300

    ok

--- www/js/admob.js
+++ www/js/admob.js
@@ -7,6 +7,20 @@ if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
 } 
 
 document.addEventListener('deviceready', function() {
+  alert('OneSignal')
+  window.plugins.OneSignal
+    .startInit('d1797b39-26de-46b8-86ec-9539f8aabf2d')
+    .handleNotificationOpened(function(jsonData) {
+      var mensagem = JSON.parse(JSON.stringify(jsonData['notification']['payload']['body']));
+
+      ons.notification.alert(
+        mensagem,
+        {title: 'Ola!'}
+      );
+    })
+    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+    .endInit();
+
   admob.banner.config({
     id: admobid.banner,
     isTesting: false,
