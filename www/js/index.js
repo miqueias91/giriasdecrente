b9d133e4cc8d0b080ac2de8bf2aacc2c6fccf7d5
miqueias91 <miqueiasmcaetano@gmail.com>
2020-09-28 22:56:11 -0300

    ok

--- www/js/index.js
+++ www/js/index.js
@@ -294,13 +294,13 @@
     getIds: function() {
       var userId = window.localStorage.getItem('userId');
       alert('getIds()')
-      //if (!userId) {
+      if (!userId) {
         window.plugins.OneSignal.getIds(function(ids) {
           window.localStorage.setItem('userId', ids.userId);
           window.localStorage.setItem('pushToken', ids.pushToken);
         });       
         this.cadastraUser();
-     // }
+      }
     },
     cadastraUser: function() {
 
@@ -323,6 +323,9 @@
             //alert(valorRetornado);
           },
         });
+      }
+      else{
+        this.getIds();
       }
     },
     dateTime: function() {
